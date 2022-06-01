eee05583ca4ec39468b643a7625ee2aa91d535a3
Jason Chew <jrchew15@gmail.com>
2022-06-01 13:26:01 -0700

    phase 1 dragons class

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,23 @@
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
 
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragons) {
+    return dragons.map((dragon) => dragon.name);
+  }
+}
+
+// const puff = new Dragon("Puff", "green");
+// const trogdor = new Dragon("Trogdor", "more green");
+// console.log(puff);
+// console.log(puff.breathesFire());
+// console.log(Dragon.getDragons(puff, trogdor));
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
